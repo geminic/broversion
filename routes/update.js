3887d2bc4779e14a895b8fa7e246a98ceedb5307
Sergey Kozlov <sk@sergeykozlov.me>
2021-06-19 22:05:12 +0400

    write timestamp of update to json file

--- routes/update.js
+++ routes/update.js
@@ -3,7 +3,9 @@
  */
 const express = require('express');
 const router = express.Router();
+const path = require('path');
 const { exec } = require('child_process');
+const fs = require('fs');
 
 router.get('/', (req, res) => {
     /**
@@ -13,11 +15,30 @@ router.get('/', (req, res) => {
         if (error) {
             res.send(error);
         } else if (stdout) {
+            writeLog(stdout);
             res.send(stdout);
         } else {
             res.send('Error');
         }
     });
 });
+
+
+/**
+ * In case of succeed update write result to json file
+ */
+function writeLog(data) {
+    const obj = {
+        date: new Date().getTime()
+    }
+
+    fs.writeFile(
+        path.resolve(__dirname, '../update_log.json'),
+        JSON.stringify(obj),
+        () => {
+            console.log('JSON data is saved.')
+        }
+    );
+}
 
 module.exports = router;
