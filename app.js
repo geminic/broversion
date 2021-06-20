573fa7fc89fddcfdad90a6c124259fea585b5c1b
Sergey Kozlov <sk@sergeykozlov.me>
2021-06-20 20:06:32 +0400

    removed ssl redirect

--- app.js
+++ app.js
@@ -4,8 +4,6 @@ const PORT = process.env.PORT || 1010;
 const routerIndex = require('./routes/index');
 const routerGetbro = require('./routes/getbro');
 const routerUpdate = require('./routes/update');
-const http = require('http');
-const enforce = require('express-sslify');
 
 
 /**
@@ -25,8 +23,6 @@ app.use('/update', routerUpdate);
 /**
  * Launch server
  */
-app.use(enforce.HTTPS({ trustProtoHeader: true }));
-
-http.createServer(app).listen(PORT, () => {
+app.listen(PORT, () => {
     console.log('Server has been started');
 });
