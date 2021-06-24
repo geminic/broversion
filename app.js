a9008145ccc9960d29fe24081fb4285bd5e1dc92
Sergey Kozlov <sk@sergeykozlov.me>
2021-06-24 14:23:08 +0400

    Enforce https for heroku

--- app.js
+++ app.js
@@ -1,9 +1,19 @@
 const express = require('express');
 const app = express();
+const http = require('http');
+const enforce = require('express-sslify');
 const PORT = process.env.PORT || 1010;
 const routerIndex = require('./routes/index');
 const routerGetbro = require('./routes/getbro');
 const routerUpdate = require('./routes/update');
+const env = process.env.NODE_ENV;
+
+/**
+ * Enforce https for heroku
+ */
+if (env !== 'dev') {
+    app.use(enforce.HTTPS({ trustProtoHeader: true }));
+}
 
 
 /**
@@ -23,6 +33,12 @@ app.use('/update', routerUpdate);
 /**
  * Launch server
  */
-app.listen(PORT, () => {
-    console.log('Server has been started');
-});
+if (env !== 'dev') {
+    http.createServer(app).listen(PORT, () => {
+        console.log('Server has been started');
+    });
+} else {
+    app.listen(PORT, () => {
+        console.log('Server has been started');
+    });
+}
